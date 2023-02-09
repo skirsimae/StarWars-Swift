a275d53ef86a05dfded0842717b09d88059e9239
Silva Kirsimae <silvakirsimae@gmail.com>
2023-02-09 15:14:48 +0100

    Setup Film model

---
+++ StarWars/Models/Film.swift
@@ -0,0 +1,51 @@
+//
+//  Films.swift
+//  StarWars
+//
+//  Created by Silva Kirsimae on 09/02/2023.
+//
+
+import Foundation
+
+struct Film: Codable {
+    let count: Int
+    let next, previous: Int
+    let results: [Result]
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.count = try container.decode(Int.self, forKey: .count)
+        self.results = try container.decode([Result].self, forKey: .results)
+        
+        if let next = try container.decodeIfPresent(Int.self, forKey: .next) {
+            self.next = next
+        } else {
+            self.next = 0
+        }
+        
+        if let previous = try container.decodeIfPresent(Int.self, forKey: .next) {
+            self.previous = previous
+        } else {
+            self.previous = 0
+        }
+    }
+}
+
+struct Result: Codable {
+    let title: String
+    let episodeID: Int
+    let openingCrawl, director, producer, releaseDate: String
+    let characters, planets, starships, vehicles: [String]
+    let species: [String]
+    let created, edited: String
+    let url: String
+
+    enum CodingKeys: String, CodingKey {
+        case title
+        case episodeID = "episode_id"
+        case openingCrawl = "opening_crawl"
+        case director, producer
+        case releaseDate = "release_date"
+        case characters, planets, starships, vehicles, species, created, edited, url
+    }
+}
