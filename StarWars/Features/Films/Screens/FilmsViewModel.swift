035d41a22f02e7e1fad4d7e43940cb9bbe30ce9a
Silva Kirsimae <silvakirsimae@gmail.com>
2023-02-10 14:10:34 +0100

    Configure Films viewmodel

--- StarWars/Features/Films/Screens/FilmsViewModel.swift
+++ StarWars/Features/Films/Screens/FilmsViewModel.swift
@@ -10,4 +10,47 @@ import RxCocoa
 
 class FilmsViewModel {
     
+    private let service: Networking
+    private let disposeBag = DisposeBag()
+    
+    init(endpoint: Driver<Endpoint>, service: Networking) {
+        self.service = service
+        
+        endpoint
+            .drive(onNext: { [weak self] endpoint in
+                self?.fetchFilms(endpoint: endpoint)
+            }).disposed(by: disposeBag)
+    }
+    
+    private let _films = BehaviorRelay<[Film]>(value: [])
+    
+    var films: Driver<[Film]> {
+        return _films.asDriver()
+    }
+    
+    var numberOfFilms: Int {
+        return _films.value.count
+    }
+    
+    func viewModelForFilm(at index: Int) -> FilmCellViewModel? {
+        guard index < _films.value.count else {
+            return nil
+        }
+        return FilmCellViewModel(film: _films.value[index])
+    }
+    
+    private func fetchFilms(endpoint: Endpoint) {
+        self._films.accept([])
+        self._isFetching.accept(true)
+        self._error.accept(nil)
+        
+        service.fetchFilms(from: endpoint, params: nil, successHandler: {[weak self] (response) in
+            self?._isFetching.accept(false)
+            self?._films.accept(response.results)
+            
+        }) { [weak self] (error) in
+            self?._isFetching.accept(false)
+            self?._error.accept(error.localizedDescription)
+        }
+    }
 }
